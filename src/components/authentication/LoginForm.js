a0e9be6d938d5e6a1e46c749e261651c3985f4c1
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-08-12 04:32:51 +0530

    feat(loginForm): add form validations

--- src/components/authentication/LoginForm.js
+++ src/components/authentication/LoginForm.js
@@ -2,15 +2,67 @@ import React from 'react'
 
 import './LoginForm.css'
 
+const FormErrors = ({ formErrors }) => (
+  <div className="form-errors">
+    {Object.keys(formErrors).map((fieldName, index) => {
+      return !!formErrors[fieldName].length > 0 ? (
+        <p style={{ color: '#FF3333' }} key={index}>
+          {fieldName} {formErrors[fieldName]}
+        </p>
+      ) : (
+        ''
+      )
+    })}
+  </div>
+)
+
 class LoginForm extends React.Component {
   state = {
     username: '',
     password: '',
+    formErrors: { username: '', password: '' },
+    usernameValid: false,
+    passwordValid: false,
+    formValid: false,
+  }
+
+  validateField = (fieldName, value) => {
+    let { formErrors, usernameValid, passwordValid } = this.state
+
+    switch (fieldName) {
+      case 'username':
+        usernameValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+        formErrors.username = !!usernameValid ? '' : ' is invalid'
+        break
+      case 'password':
+        passwordValid = value.length > 0
+        formErrors.password = !!passwordValid ? '' : ` can't be empty`
+        break
+      default:
+        break
+    }
+
+    this.setState(
+      {
+        formErrors,
+        usernameValid,
+        passwordValid,
+      },
+      this.validateForm
+    )
+  }
+
+  validateForm = _ => {
+    this.setState({
+      formValid: this.state.usernameValid && this.state.passwordValid,
+    })
   }
 
   handleInputChange = event => {
     const { value, name } = event.target
-    this.setState({ [name]: value })
+    this.setState({ [name]: value }, () => {
+      this.validateField(name, value)
+    })
   }
 
   render() {
@@ -30,7 +82,6 @@ class LoginForm extends React.Component {
             placeholder="Username"
             value={this.state.username}
             onChange={this.handleInputChange}
-            required
           />
           <label htmlFor="password" />
           <input
@@ -40,7 +91,6 @@ class LoginForm extends React.Component {
             placeholder="Password"
             value={this.state.password}
             onChange={this.handleInputChange}
-            required
           />
           <input
             type="submit"
@@ -48,7 +98,9 @@ class LoginForm extends React.Component {
             name="login"
             alt="Login"
             value="Login"
+            disabled={!this.state.formValid}
           />
+          <FormErrors formErrors={this.state.formErrors} />
         </form>
       </div>
     )
