4d4fffb4d499200d2850ed39f6867ecfb2fb406d
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-08-12 03:58:53 +0530

    style(loginForm): remove the li hack for block behaviour

--- src/components/authentication/LoginForm.js
+++ src/components/authentication/LoginForm.js
@@ -22,41 +22,33 @@ class LoginForm extends React.Component {
         </div>
         <form className="login-form" onSubmit={this.props.submitForm}>
           <h2>Login</h2>
-          <ul className="noBullet">
-            <li>
-              <label htmlFor="username" />
-              <input
-                type="text"
-                className="inputFields"
-                name="username"
-                placeholder="Username"
-                value={this.state.username}
-                onChange={this.handleInputChange}
-                required
-              />
-            </li>
-            <li>
-              <label htmlFor="password" />
-              <input
-                type="password"
-                className="inputFields"
-                name="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.handleInputChange}
-                required
-              />
-            </li>
-            <li id="center-btn">
-              <input
-                type="submit"
-                id="login-btn"
-                name="login"
-                alt="Login"
-                value="Login"
-              />
-            </li>
-          </ul>
+          <label htmlFor="username" />
+          <input
+            type="text"
+            className="inputFields"
+            name="username"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            required
+          />
+          <label htmlFor="password" />
+          <input
+            type="password"
+            className="inputFields"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+            required
+          />
+          <input
+            type="submit"
+            id="login-btn"
+            name="login"
+            alt="Login"
+            value="Login"
+          />
         </form>
       </div>
     )
